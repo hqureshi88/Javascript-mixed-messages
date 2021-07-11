7f03371b7ef95165dcf90e5b63d87c6d8fec7c41
hamid <hamidq88@gmail.com>
2021-07-11 23:38:14 +0100

    add comments

--- jokeMessageGenerator.js
+++ jokeMessageGenerator.js
@@ -1,13 +1,15 @@
 //Joke Message Generator
 
+// below random function to generate index number to messages variable
 function random(numLength) {
 let randNum = Math.floor((Math.random()*numLength));
 return randNum; 
 }
 
-// let testNum = random(10);
-
+//messageGenerator to start of the program
 function messageGenerator() {
+    
+    //Array of objects containing key/values
     const messages = [
         {Figs: 'Figs the doorbell, it’s not working!'},
         {Luke: 'Luke through the peep hole and find out.'},
@@ -15,13 +17,12 @@ function messageGenerator() {
         {Annie: 'Annie thing you can do, I can do too!'}
     ]
 
+    //call for random function for index number
     let randomIndex = random(messages.length);
-
-    // console.log(randomIndex);
-    // console.log(messages[randomIndex]);
 
     let messageObject = messages[randomIndex];
     
+    //once array selected from object, then output key/value contents in string literal format
     for (const [key, value] of Object.entries(messageObject)) {
         let output = `Me: Knock Knock,\nYou: Whose there?,\nMe: ${key},\nYou: ${key} who?\nMe: ${value}`;
         return output;
@@ -29,5 +30,6 @@ function messageGenerator() {
 
 }
 
+//calls messageGenerator function and outputs results via console log
 console.log(messageGenerator());
 
